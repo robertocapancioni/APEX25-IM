3db53f42ee7ad75988afaeb4e91653bb4db23086
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-01-29 11:23:54 +0100

    Update d03_insert.sql

--- D03/d03_insert.sql
+++ D03/d03_insert.sql
@@ -3,3 +3,21 @@ insert into EMP ( EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM, DEPTNO)
 
 insert into EMP 
      values     ( 8003,'BIANCHI','IMPIEGATO',7782,date'2024-03-18',1500,null,null); 
+
+
+insert into EMP ( EMPNO
+                 ,ENAME
+                 ,JOB
+                 ,MGR
+                 ,HIREDATE
+                 ,SAL
+                 ,COMM
+                 ,DEPTNO ) 
+         values ( 8001
+                 ,'VERDI'
+                 ,'IMPIEGATO'
+                 ,(select EMPNO from EMP where ENAME='BLAKE')
+                 ,date'2024-03-18'
+                 ,1500
+                 ,null
+                 , (select DEPTNO from DEPT where DNAME='ACCOUNTING')); 
