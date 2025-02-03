d023cf64db818d8914cb87fdade36c3c590e202d
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-02-03 11:54:54 +0100

    Update grouping_sets.sql

--- D05/grouping_sets.sql
+++ D05/grouping_sets.sql
@@ -10,3 +10,20 @@
     (cliente),
     ()
   );
+
+select zona_cliente,
+       cliente,
+       grouping(zona_cliente)                   raggr_zona_cliente,
+       grouping(cliente)                        raggr_cliente,
+       grouping(zona_cliente)*grouping(cliente) raggr_zona_cliente_cliente,
+       sum(quantita) quantita, 
+       sum(importo) importo
+  from d05_vendita
+ group by 
+     grouping sets(
+       (zona_cliente,cliente),
+       (zona_cliente),
+       (cliente),
+       ()
+     )
+order by zona_cliente,cliente;
