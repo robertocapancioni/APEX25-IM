4144e43501b8b56a72b5ebf9a3d034102521adaa
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-02-03 12:05:00 +0100

    Update grouping_sets.sql

--- D05/grouping_sets.sql
+++ D05/grouping_sets.sql
@@ -41,3 +41,16 @@ select zona_cliente,
  group by 
      cube(zona_cliente,cliente)
 order by zona_cliente,cliente;
+
+
+select zona_cliente,
+       cliente,
+       grouping(zona_cliente)                   raggr_zona_cliente,
+       grouping(cliente)                        raggr_cliente,
+       grouping(zona_cliente)*grouping(cliente) raggr_zona_cliente_cliente,
+       sum(quantita) quantita, 
+       sum(importo) importo
+  from d05_vendita
+ group by 
+     rollup(zona_cliente,cliente)
+order by zona_cliente,cliente;
