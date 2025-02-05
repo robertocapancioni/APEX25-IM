cdb84efcdcb5ec825528b3407320b5949a4fa983
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-02-05 11:48:40 +0100

    Update d06_my_pkg.sql

--- D06/d06_my_pkg.sql
+++ D06/d06_my_pkg.sql
@@ -1,37 +1,10 @@
 create or replace package d06_my_pkg
 is
-    procedure my_proc (
-        p_param1                      in number,
-        p_param2                     out number
-    );
+procedure venduto(p_venduto in number default 0);
+procedure venduto(p_venduto in number default 0,
+                      p_venduto_txt out varchar2);
 
-    function my_func (
-        p_param1                      in number,
-        p_param2                      in number
-    ) return number;
-end d06_my_pkg;
-/
-
-
-create or replace package  body d06_my_pkg
-is
-    procedure my_proc (
-        p_param1                      in number,
-        p_param2                     out number
-    )
-    is
-
-    begin
-       p_param2 := 2 * p_param1;
-    end my_proc;
-
-    function my_func (
-        p_param1                      in number,
-        p_param2                      in number
-    ) return number
-    is
-    begin
-        return p_param1 + p_param2;
-    end my_func;
+function get_venduto(p_venduto in number default 0)
+return varchar2;
 end d06_my_pkg;
 /
