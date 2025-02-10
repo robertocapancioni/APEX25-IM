983c1c1b4c1bbdd9706533ff87d924fbbd72adf8
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-02-10 11:48:03 +0100

    Create d07_views.sql

---
+++ D07/d07_views.sql
@@ -0,0 +1,18 @@
+create or replace view d07_acquisto_vw as
+select a.id,
+       a.fornitore_id,
+       f.fornitore,  
+       a.prodotto_id,
+       p.prodotto,
+       p.prezzo_acquisto,
+       p.tipo_prodotto_id,
+       tp.tipo_prodotto,
+       a.data,
+       a.quantita,
+       a.quantita*p.prezzo_acquisto importo
+  from d07_acquisto a
+  join d07_fornitore f on a.fornitore_id = f.id
+  join d07_prodotto p on a.prodotto_id = p.id
+  join d07_tipo_prodotto tp on p.tipo_prodotto_id = tp.id;
+
+  
