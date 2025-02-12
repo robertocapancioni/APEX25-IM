8e4977ab20b52c3c58d3f010bcb475fc05d63132
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-02-12 11:21:31 +0100

    Update d07_cursor_emp.sql

--- D07/d07_cursor_emp.sql
+++ D07/d07_cursor_emp.sql
@@ -14,3 +14,20 @@ BEGIN
    CLOSE c_emps; 
 END; 
 /
+
+
+
+DECLARE 
+   c_emp emp%rowtype;
+   CURSOR c_emps is 
+      SELECT EMPNO, ENAME, JOB FROM emp; 
+BEGIN 
+   OPEN c_emps; 
+   LOOP 
+   FETCH c_emps into c_emp.EMPNO, c_emp.ENAME, c_emp.JOB; 
+      EXIT WHEN c_emps%notfound; 
+      dbms_output.put_line(c_emp.EMPNO || ' ' || c_emp.ENAME || ' ' || c_emp.JOB); 
+   END LOOP; 
+   CLOSE c_emps; 
+END; 
+/
