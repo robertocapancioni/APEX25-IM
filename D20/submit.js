3fabbe9facfbaeec1ba099695a3a1f3fc6291ca5
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-03-26 09:59:09 +0100

    Add files via upload

---
+++ D20/submit.js
@@ -0,0 +1,9 @@
+apex.submit({request:"CREATE",
+    set:{"P3_TIPO_DOCUMENTO":"X001"}
+   });
+
+apex.submit({request:"SAVE",
+    set:{"P3_TIPO_DOCUMENTO":"X001"}
+   });
+
+apex.submit({request:"DELETE"});
