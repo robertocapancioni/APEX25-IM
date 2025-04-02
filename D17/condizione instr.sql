308a52f000821c40b2910f797978e6e7c7ce81da
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-04-02 10:42:14 +0200

    Create condizione instr.sql

---
+++ D17/condizione instr.sql
@@ -0,0 +1,2 @@
+where (instr( ':' || :P7_NAZIONE  || ':', ':' || nazione   || ':' ) > 0 or :P7_NAZIONE is null)
+   and (instr( ':' || :P7_SETTORE  || ':', ':' || settore   || ':' ) > 0 or :P7_SETTORE is null)
