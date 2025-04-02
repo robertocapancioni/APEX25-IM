9a17db40aeba6229e78dfbaf07e7f2dcd56bdbbc
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-04-02 09:58:47 +0200

    Add files via upload

---
+++ D21/d21_ig_options.js
@@ -0,0 +1,7 @@
+function(config) {
+    config.defaultGridViewOptions = {
+        selectionStateItem: "P01_SELECTED_IDS", // Sostituisci con il tuo elemento di pagina
+        multiple: true // Permette la selezione multipla
+    };
+    return config;
+}
