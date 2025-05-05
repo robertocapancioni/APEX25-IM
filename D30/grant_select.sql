42e110660c2e5bff72e4bf77be58c3339c2a8ca6
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-05-05 11:26:08 +0200

    Update grant_select.sql

--- D30/grant_select.sql
+++ D30/grant_select.sql
@@ -1,15 +1,15 @@
-CREATE PROCEDURE grant_select(
-    username VARCHAR2, 
-    grantee VARCHAR2)
+CREATE PROCEDURE grant_select_all_tables(
+    from_user VARCHAR2, 
+    to_user VARCHAR2)
 AS   
 BEGIN
     FOR r IN (
         SELECT owner, table_name 
         FROM all_tables 
-        WHERE owner = username
+        WHERE owner = from_user
     )
     LOOP
         EXECUTE IMMEDIATE 
-            'GRANT SELECT ON '||r.owner||'.'||r.table_name||' to ' || grantee;
+            'GRANT SELECT ON '||r.owner||'.'||r.table_name||' to ' || to_user;
     END LOOP;
 END; 
