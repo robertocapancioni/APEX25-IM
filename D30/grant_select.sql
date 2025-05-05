6cdb3a4c898aeca1a4dcf9eb4aba54fb02d3bdcb
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-05-05 09:35:28 +0200

    Create grant_select.sql

---
+++ D30/grant_select.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE grant_select(
+    username VARCHAR2, 
+    grantee VARCHAR2)
+AS   
+BEGIN
+    FOR r IN (
+        SELECT owner, table_name 
+        FROM all_tables 
+        WHERE owner = username
+    )
+    LOOP
+        EXECUTE IMMEDIATE 
+            'GRANT SELECT ON '||r.owner||'.'||r.table_name||' to ' || grantee;
+    END LOOP;
+END; 
